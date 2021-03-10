a6b078d04164362bb4101cb09f5f0d6c07662984
мытя пупкин <80246817+oTvErTkA-tech@users.noreply.github.com>
2021-03-10 08:57:22 +0200

    Create Program.cs

---
+++ NetCoreNewProj/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NetCoreNewProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool success = false;
+            Console.WriteLine("Введите число");
+            
+            do
+            {              
+                string answer = Console.ReadLine();
+                Console.Clear();
+                success = int.TryParse(answer, out int value);
+                if (value >= 1 && value <= 10)
+                {
+                    Console.WriteLine("Ваше число: " + value);
+                }
+                if(!success)
+                {
+                    Console.WriteLine("Вы ввели не число");
+                }
+                
+            } while (!success);
+            
+        }
+    }
+}
