edd40124cd2cc902a7529599a2f835375c0d9dca
мытя пупкин <80246817+oTvErTkA-tech@users.noreply.github.com>
2021-03-17 17:27:41 +0200

    Create Printer.cs

---
+++ ochko/Printer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ochko
+{
+    public static class Printer
+    {
+        public static void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }    
+}
