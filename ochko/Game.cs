a327eef2a36d4e5a87707480c729dc102cddc9b6
мытя пупкин <80246817+oTvErTkA-tech@users.noreply.github.com>
2021-03-17 17:27:43 +0200

    Create Game.cs

---
+++ ochko/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ochko
+{
+    public class Game
+    {
+        private Player _player;
+        public Game()
+        {
+            _player = new Player();
+            
+
+        }
+    }
+    public class Player
+    {
+        private string _name;
+    }
+}
